6c1764a6e6978df942aad754696e251cc2acca8b
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-07-06 14:28:24 -0400

    lib

---
+++ lib/app.d.ts
@@ -0,0 +1,7 @@
+import { SocksClientOptions } from "socks";
+export default class ImapClient {
+    private options;
+    private socket;
+    constructor(options: SocksClientOptions);
+    connect(): Promise<void>;
+}
